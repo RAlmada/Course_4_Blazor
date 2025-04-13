4c65d3f085c442155a64816fc4b1a9c2149cbc7a
Robert Almada <RAlmada@users.noreply.github.com>
2025-04-12 22:40:57 -0700

    Some additional registration fields initializations

--- Blazor_Project/EventEase/EventEase/Shared/Models/RegistrationModel.cs
+++ Blazor_Project/EventEase/EventEase/Shared/Models/RegistrationModel.cs
@@ -4,6 +4,12 @@ namespace EventEase.Shared.Models;
 
 public class RegistrationModel
 {
+    public static int RegistrationIdCounter = 0;
+    public static int GetNextRegistrationId()
+    {
+        return ++RegistrationIdCounter;
+    }
+
     public int Id { get; set; }
     [Required(ErrorMessage = "Name is required")]
     public string Name { get; set; }
@@ -16,7 +22,10 @@ public class RegistrationModel
     public DateTime RegistrationDate { get; set; }
     public int EventId { get; set; }
 
-    public RegistrationModel() : this(0, string.Empty, string.Empty, string.Empty, DateTime.MinValue, 0) { }
+    public RegistrationModel() : this(GetNextRegistrationId(), string.Empty, string.Empty, string.Empty, DateTime.MinValue, 0) { }
+
+    public RegistrationModel(DateTime registrationDate, int eventId)
+        : this(GetNextRegistrationId(), string.Empty, string.Empty, string.Empty, registrationDate, eventId) { }
 
     public RegistrationModel(int id, string name, string email, string phoneNumber, DateTime registrationDate, int eventId)
     {
